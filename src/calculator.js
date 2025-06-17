3e72b30d2922279d0f16f37279a067a53099ebe8
Cristian Ordoñez <cordonezjolon@gmail.com>
2025-06-16 22:31:44 -0600

    feat: add square operation to calculator

--- src/calculator.js
+++ src/calculator.js
@@ -11,5 +11,8 @@ function divide(a, b) {
   if (b === 0) throw new Error('Cannot divide by zero');
   return a / b;
 }
+function square(a) {
+  return a * a;
+}
 
-module.exports = { add, subtract, multiply, divide };
+module.exports = { add, subtract, multiply, divide, square };
